fe90bb7748d75f64ae6da0aa4e3a364a25f49aff
daboss0580 <iamjustin0580@gmail.com>
2023-04-20 19:51:57 +0100

    Commiting my Homework

--- src/Q10.java
+++ src/Q10.java
@@ -4,7 +4,14 @@ public class Q10 {
         Write a program to print out duplicate elements from
 an Array of Strings
          */
-        int a,b;
-        String []  name= {"Asghar", "Syntax","Pratyush", "Sia", "Sumair", "Mozzam"};
+
+        String []  name= {"Asghar", "Syntax","Pratyush", "Sia", "Sumair", "Mozzam", "Pratyush"};
+        for (int a=0; a<name.length; a++){
+            for (int b=a+1; b<name.length; b++){
+                if (name[a].equals(name[b])){
+                    System.out.println(name[b]);
+                }
+            }
+        }
     }
 }
